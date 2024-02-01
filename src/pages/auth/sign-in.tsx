0bdfe7809aef539ea3e9e75fb721299640f3895f
Clelton Mielke <cleltonjm_95@hotmail.com>
2024-01-31 22:49:45 -0300

    Cadastro de restaurante

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { toast } from 'sonner'
+import { Link } from 'react-router-dom'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
@@ -37,6 +38,10 @@ export function SignIn() {
         <>
             <Helmet title="Login" />
             <div className="p-8">
+                <Button variant="ghost" asChild className="absolute right-8 top-8">
+                    <Link to="/sign-up">Novo estabelecimento</Link>
+                </Button>
+
                 <div className="w-[320] flex flex-col justify-center gap-6">
                     <div className="flex flex-col gap-2 text-center">
                         <h1 className="text-2xl font-semibold tracking-tight">
