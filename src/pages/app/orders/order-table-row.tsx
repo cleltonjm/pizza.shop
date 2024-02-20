9ed06d639de434d5aa80ffa125e03b38808f3397
Clelton Mielke <cleltonjm_95@hotmail.com>
2024-02-19 21:40:13 -0300

    Filtros de pedido

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { ArrowRight, X, Search } from "lucide-react";
+
+export function OrderTableRow() {
+    return (
+        <TableRow>
+            <TableCell>
+                <Button variant="outline" size="xs">
+                    <Search className="h-3 w-3" />
+                    <span className="sr-only">Detalhes do pedido</span>
+                </Button>
+            </TableCell>
+            <TableCell className="font-mono text-xs font-medium">a698s4d56asd1as65d1</TableCell>
+            <TableCell className="text-muted-foreground">
+                há 15 minutos
+            </TableCell>
+            <TableCell>
+                <div className="flex items-center gap-2">
+                    <span className="h-2 w-2 rounded-full bg-slate-400" />
+                    <span className="font-medium text-muted-foreground">Pendente</span>
+                </div>
+            </TableCell>
+            <TableCell className="font-medium">Clelton José Mielke</TableCell>
+            <TableCell className="font-medium">R$ 149,90</TableCell>
+            <TableCell>
+                <Button variant="outline" size="xs">
+                    <ArrowRight className="h-3 w-3 mr-2" />
+                    Aprovar
+                </Button>
+            </TableCell>
+            <TableCell className="">
+                <Button variant="ghost" size="xs">
+                    <X className="h-3 w-3 mr-2" />
+                    Cancelar
+                </Button>
+            </TableCell>
+        </TableRow>
+    )
+}
