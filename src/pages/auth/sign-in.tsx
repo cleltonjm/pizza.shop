970f89bdcb2ddc66138e7292ab50f263dec71ead
Clelton Mielke <cleltonjm_95@hotmail.com>
2024-03-26 22:17:10 -0300

    Authenticacao do usuario

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -7,6 +7,8 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Helmet } from 'react-helmet-async'
+import { useMutation } from '@tanstack/react-query'
+import { signIn } from '@/api/sign-in'
 
 const signInForm = z.object({
     email: z.string().email(),
@@ -17,11 +19,13 @@ type SignInForm = z.infer<typeof signInForm>
 export function SignIn() {
     const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
 
+    const { mutateAsync: authenticate } = useMutation({
+        mutationFn: signIn,
+    })
+
     async function handleSignIn(data: SignInForm) {
         try {
-            console.log(data)
-
-            await new Promise(resolve => setTimeout(resolve, 2000))
+            await authenticate({ email: data.email })
 
             toast.success('Enviamos um link de autenticação para seu e-mail.', {
                 action: {
