dabd2315ffbe890ad41e4a91db559915e1c25f9b
Clelton Mielke <cleltonjm_95@hotmail.com>
2024-04-23 20:48:11 -0300

    Filtro dos pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,7 +1,7 @@
 import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Helmet } from "react-helmet-async";
 import { OrderTableRow } from "./order-table-row";
-import { OrdderTableFilters } from "./order-table-filters";
+import { OrderTableFilters } from "./order-table-filters";
 import { Pagination } from "@/components/pagination";
 import { useQuery } from "@tanstack/react-query";
 import { getOrders } from "@/api/get-orders";
@@ -11,14 +11,18 @@ import { z } from "zod";
 export function Orders() {
     const [searchParams, setSearchParams] = useSearchParams()
 
+    const orderId = searchParams.get('orderId')
+    const customerName = searchParams.get('customerName')
+    const status = searchParams.get('status')
+
     const pageIndex = z.coerce
         .number()
         .transform(page => page - 1)
         .parse(searchParams.get('page') ?? '1')
 
     const { data: result } = useQuery({
-        queryKey: ['orders', pageIndex],
-        queryFn: () => getOrders({ pageIndex }),
+        queryKey: ['orders', pageIndex, orderId, customerName, status],
+        queryFn: () => getOrders({ pageIndex, orderId, customerName, status: status === 'all' ? null : status, }),
     })
 
     function handlePaginate(pageIndex: number) {
@@ -36,7 +40,7 @@ export function Orders() {
                 <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
             
                 <div className="space-y-2.5">
-                    <OrdderTableFilters />
+                    <OrderTableFilters />
 
                     <div className="border rounded-md">
                         <Table>
